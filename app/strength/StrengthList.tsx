a88672e06058408068968f6fbfd072d11c848374
Casey DeMattina <cdemattina@gmail.com>
2025-12-13 18:30:18 -0500

    save wip

---
+++ app/strength/StrengthList.tsx
@@ -0,0 +1,7 @@
+export default function StrengthList() {
+    return (
+        <section>
+            this is the strength list
+        </section>
+    )
+}
--- app/strength/page.tsx
+++ app/strength/page.tsx
@@ -1,10 +1,16 @@
 "use server";
 import StrengthProvider from "./StrengthProvider";
+import { getStrengthWorkouts } from "../actions/strength";
 export default async function StrengthPage() {
+    const workouts = await getStrengthWorkouts();
+    console.log("Strength workouts fetched:", workouts);
     return (
         <main>
             <h1>Strength Workouts</h1>
+            <StrengthProvider>
+                {/* Future components like AddWorkout and StrengthList will go here */}
             <p>Welcome to the Strength section! Here you'll find various strength training workouts to build muscle and improve overall fitness.</p>
+            </StrengthProvider>
         </main>
     )
 }
