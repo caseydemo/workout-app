4cfa4fb8392f7c6c2194cc2d0996eeae7943856f
Casey DeMattina <cdemattina@gmail.com>
2025-12-13 19:18:04 -0500

    create very basic AddWorkout component for strength

---
+++ app/strength/AddWorkout.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function AddWorkout() {
+    return (
+        <section>
+            this is add workout
+        </section>
+    )
+}
--- app/strength/page.tsx
+++ app/strength/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 import StrengthProvider from "./StrengthProvider";
 import StrengthList from "./StrengthList";
+import AddWorkout from "./AddWorkout";
 import { getStrengthWorkouts } from "../actions/strength";
 export default async function StrengthPage() {
     const workouts = await getStrengthWorkouts();
@@ -9,6 +10,7 @@ export default async function StrengthPage() {
         <main>
             <h1>Strength Workouts</h1>
             <StrengthProvider>
+            <AddWorkout />
             <StrengthList />
             <p>Welcome to the Strength section! Here you'll find various strength training workouts to build muscle and improve overall fitness.</p>
             </StrengthProvider>
